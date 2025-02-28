15cf3310335d38f5ffb61d670032e9e49f3b478d
StasPonomaryov <ponomaryov.stas@gmail.com>
2025-02-28 08:03:15 +0200

    feat: add telegram web app script; update pages;

---
+++ app/(home)/about/page.tsx
@@ -0,0 +1,10 @@
+function AboutPage() {
+  return (
+    <div className="page home">
+      <h2 className="text-xl my-2">About</h2>
+      <p>This is About page</p>
+    </div>
+  )
+}
+
+export default AboutPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { cookies } from "next/headers";
+import Script from 'next/script';
 import { Item } from "@/types";
 import ItemElement from "@/components/item";
 
 export default async function Home() {
   const coockieStore = await cookies();
-  const authToken = coockieStore.get("firebaseIdToken")?.value;
+  const authToken = coockieStore.get("firebaseIdToken")?.value || '';
   let items: Item[] = [];
   const response = await fetch(`${process.env.API_URL}/api/items`, {
     method: "GET",
@@ -22,6 +23,7 @@ export default async function Home() {
 
   return (
     <div className="page home">
+      <Script src="https://telegram.org/js/telegram-web-app.js?1" />
       <h2 className="text-xl my-2">Home</h2>
       {items?.length ? (
         items.map((item) => (
